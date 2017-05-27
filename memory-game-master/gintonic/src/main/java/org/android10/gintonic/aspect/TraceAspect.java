55b8f3bac08eb465596d453f1aeaf91c8b46d5bf
ZhengHanHo <759553896@qq.com>
2017-05-27 14:55:50 +0800

    source code

---
+++ memory-game-master/gintonic/src/main/java/org/android10/gintonic/aspect/TraceAspect.java
@@ -0,0 +1,62 @@
+package org.android10.gintonic.aspect;
+
+
+import org.android10.gintonic.internal.DebugLog;
+import org.android10.gintonic.internal.StopWatch;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+
+@Aspect
+public class TraceAspect {
+    String[] f=new String[100];
+    int flag=-1;
+
+    private static final String POINTCUT_METHOD =
+            "execution(@org.android10.gintonic.annotation.DebugTrace * *(..))";
+
+    private static final String POINTCUT_CONSTRUCTOR =
+            "execution(@org.android10.gintonic.annotation.DebugTrace *.new(..))";
+
+    @Pointcut(POINTCUT_METHOD)
+    public void methodAnnotatedWithDebugTrace() {}
+
+    @Pointcut(POINTCUT_CONSTRUCTOR)
+    public void constructorAnnotatedDebugTrace() {}
+
+
+
+    @Around("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
+    public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object result = joinPoint.proceed();
+        stopWatch.stop();
+
+        DebugLog.log(className, buildLogMessage(methodName, stopWatch.getTotalTimeMillis()));
+
+        return result;
+    }
+
+    private static String buildLogMessage(String methodName, long methodDuration) {
+        StringBuilder message = new StringBuilder();
+        message.append(methodName);
+        message.append(" --> ");
+        message.append("[");
+        message.append(methodDuration);
+        message.append("ms");
+        message.append("]");
+
+        return message.toString();
+    }
+}
